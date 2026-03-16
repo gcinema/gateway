765a76f1b8e6518424ae040e5ab0d63b3e9aad49
Zakhar Guskov <guskov.zahar20@gmail.com>
2026-03-16 15:26:40 +0500

    feat: add cors default middleware

--- internal/http-server/auth/handler.go
+++ internal/http-server/auth/handler.go
@@ -1,19 +1,23 @@
+// Package auth
 package auth
 
 import (
 	"log/slog"
 	"net/http"
 
-	"github.com/gcinema/core/httphelper"
+	"github.com/gcinema/core/http-server/httpreq"
+	"github.com/gcinema/core/http-server/httpres"
+	"github.com/go-playground/validator/v10"
 )
 
 type AuthHandler struct {
-	router *http.ServeMux
-	logger *slog.Logger
+	router    *http.ServeMux
+	logger    *slog.Logger
+	validator *validator.Validate
 }
 
 func NewAuthHandler(mux *http.ServeMux, logger *slog.Logger) *AuthHandler {
-	handler := AuthHandler{router: mux, logger: logger}
+	handler := AuthHandler{router: mux, logger: logger, validator: validator.New()}
 	return &handler
 }
 
@@ -27,12 +31,24 @@ type Response struct {
 	Password string
 }
 
+type Request struct {
+	Name     string
+	Password string
+}
+
 func (handler *AuthHandler) sayHi() http.HandlerFunc {
 	return func(w http.ResponseWriter, req *http.Request) {
-		data := Response{
-			Name:     "Zuuuuu",
-			Password: "Psfdsaf",
+		payload, err := httpreq.DecodeAndValidateBody[Request](&w, req, handler.validator)
+		if err != nil {
+			return
 		}
-		httphelper.ConvertToJSON(w, data, 200)
+
+		handler.logger.Info("Получили запрос", payload.Name, payload.Password)
+		data := Response{
+			Name:     payload.Name,
+			Password: payload.Password,
+		}
+
+		httpres.ConvertToJSON(w, data, 200)
 	}
 }
